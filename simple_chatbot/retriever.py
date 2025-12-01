4bbed6f88e70bb78121fc5382dbe1ed7f5512b96
siggu <siggu100@gmail.com>
2025-12-01 23:03:33 +0900

    feat: retriever 추가

---
+++ simple_chatbot/retriever.py
@@ -0,0 +1,21 @@
+"""
+4. 유사도 검색
+"""
+
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+
+
+def get_retriever():
+    # 벡터 데이터베이스 설정
+    embedding = OpenAIEmbeddings(model="text-embedding-3-large")
+    index_name = "tax-markdown-index"
+
+    database = PineconeVectorStore(
+        embedding=embedding,
+        index_name=index_name,
+    )
+
+    retriever = database.as_retriever(search_kwargs={"k": 2})
+
+    return retriever
